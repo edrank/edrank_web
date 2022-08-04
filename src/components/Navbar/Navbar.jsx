049d6ba7e82bbdfb70052c479d066d3978ce41bb
sahilchalia <37237782+sahilchalia@users.noreply.github.com>
2022-08-04 21:54:50 +0530

    Navbar created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.scss';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+	return (
+		<div className='navbar-main'>
+			<div className='navbar-main-burger-menu'>
+				<i className='fa-solid fa-bars'></i>
+			</div>
+			<div className='navbar-main-logo'>EDURANK</div>
+			<div className='navbar-main-user-menu'>
+				<Link to='/profile'>
+					<i className='fa-solid fa-circle-user'></i>
+				</Link>
+				&nbsp; &nbsp; &nbsp;
+				<i className='fa-solid fa-ellipsis-vertical'></i>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
