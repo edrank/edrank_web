e611774e80398b240c7abf72639833d2dfe69d07
sahilchalia <37237782+sahilchalia@users.noreply.github.com>
2022-08-26 10:07:57 +0530

    partial commit 2

--- src/pages/FeedbackFormPage/FeedbackFormPage.jsx
+++ src/pages/FeedbackFormPage/FeedbackFormPage.jsx
@@ -28,6 +28,8 @@ function FeedbackFormPage() {
 
 	const [teachers, setteachers] = useState([]);
 	const [feedbackQuestions, setFeedbackQuestions] = useState([]);
+	const [driveId, setDriveId] = useState(0);
+	// const [teacherId, setTeacherId] = useState(0);
 
 	const getRadioOptions = question => {
 		let optionsArray = [];
@@ -97,6 +99,7 @@ function FeedbackFormPage() {
 			console.log(response?.data.data);
 			console.log(response2?.data.data);
 			setFeedbackQuestions(response?.data.data.questions);
+			setDriveId(response?.data.data.drive_id);
 			let teachersOpts = [];
 			response2.data.data.teachers.forEach(t => {
 				teachersOpts.push({
@@ -109,76 +112,152 @@ function FeedbackFormPage() {
 		fetchData();
 	}, []);
 
+	const createMcqResponse = (questionIdsArray, optionIdsArray) => {
+		let mcqResponse = [];
+		questionIdsArray.forEach((questionId, index) => {
+			mcqResponse.push({
+				question_id: questionId,
+				answer_id: optionIdsArray[index],
+			});
+		});
+
+		// mcqResponse = [...mcqResponse, { text_feedback: '' }];
+		return mcqResponse;
+	};
+
+	const changeToRequiredFormat = data => {
+		let questionIds = Object.keys(data);
+		let teacherId = data.teacher.value;
+		// setTeacherId(data.teacher.value);
+		questionIds.pop();
+		console.log('questionIds', questionIds);
+		let questionIdsArray = [];
+		questionIds.map(questionId => questionIdsArray.push(Number(questionId)));
+		console.log('questionIdsArray', questionIdsArray);
+
+		let optionIds = Object.values(data);
+		optionIds.pop();
+		console.log('OPTIONIDs');
+		console.log(optionIds);
+		let optionIdsArray = [];
+		optionIds.map(option => optionIdsArray.push(Number(option.slice(-1))));
+		console.log(optionIdsArray);
+
+		const requestBody = {
+			drive_id: driveId,
+			teacher_id: teacherId,
+			feedback: {
+				mcq: createMcqResponse(questionIdsArray, optionIdsArray),
+				text_feedback: data.text_feedback,
+			},
+		};
+		console.log('requestBody', JSON.stringify(requestBody));
+		return requestBody;
+		// console.log(requestBody);
+	};
 	const onSubmit = data => {
 		console.log('data');
 		console.log(data);
+
+		const requestBody = changeToRequiredFormat(data);
+		console.log('requestBody');
+		console.log(requestBody);
+		const response = makeRequest(
+			'android/submit-feedback/ST',
+			'POST',
+			requestBody,
+		);
+		
+
+
 	};
 
-	const SubmitForms = e => {
-		e.preventDefault();
-		document.getElementById('feedback-form-0').submit();
-		document.getElementById('feedback-form-1').submit();
-	};
+	// const SubmitForms = e => {
+	// 	e.preventDefault();
+	// 	document.getElementById('feedback-form-0').submit();
+	// 	document.getElementById('feedback-form-1').submit();
+	// };
 
 	return (
 		<div className='feedback-form-page-main'>
-			<h1 className='feedback-form-page-header'>
-				Please provide your valuable feedback
-			</h1>
-			<form id='feedback-form-1' onSubmit={handleSubmit(onSubmit)}>
-				<div className='feedback-form-page-body'>
-					{feedbackForms.map((form, i) => (
-						<Fragment key={i}>
-							{/* <FormGenerator
-							key={i}
-							formIndex={i}
-							formClass='feedback-form-wrapper'
-							formId={'feedback-form-' + i}
-							onSubmit={onSubmit}
-							formObject={getFormObject()}
-							handleRemoveForm={() => handleRemoveForm(i)}
-							isFeedbackForm={true}
-							formRegister={'register' + i}
-							formControl={'control' + i}
-							formErrors={'errors' + i}
-							formHandleSubmit={'handleSubmit' + i}
-						/> */}
-
-							<FormFieldGenerator
-								formObject={getFormObject()}
-								register={register}
-								control={control}
-								errors={errors}
-								formIndex={i}
-							/>
-
-							<div>
-								<h3>Form {i + 1}</h3>
-								<button
-									onClick={() => handleRemoveForm(i)}
-									className='remove-feedback-form'
-								>
-									Remove
-								</button>
-							</div>
-						</Fragment>
-					))}
-					<div className='form-div'>
-						<button onClick={handleAddForm} className='add-feedback-form'>
-							Add another form
-						</button>
-					</div>
-
-					{/* <div className='form-div'>
-						<button onClick={e => SubmitForms(e)}>SUBMIT</button>
-					</div> */}
-				</div>
-				<div className='form-div'>
-					<input type='submit' value='SUBMITTTT' />
-				</div>
-			</form>
+			<FormGenerator formObject={getFormObject()} onSubmit={onSubmit} />
 		</div>
 	);
+
+	// return (
+	// 	<div className='feedback-form-page-main'>
+	// 		<h1 className='feedback-form-page-header'>
+	// 			Please provide your valuable feedback
+	// 		</h1>
+	// 		<form id='feedback-form-1' onSubmit={handleSubmit(onSubmit)}>
+	// 			<div className='feedback-form-page-body'>
+	// 				{/* {feedbackForms.map((form, i) => (
+	// 					<Fragment key={i}>
+	// 						{/* <FormGenerator
+	// 						key={i}
+	// 						formIndex={i}
+	// 						formClass='feedback-form-wrapper'
+	// 						formId={'feedback-form-' + i}
+	// 						onSubmit={onSubmit}
+	// 						formObject={getFormObject()}
+	// 						handleRemoveForm={() => handleRemoveForm(i)}
+	// 						isFeedbackForm={true}
+	// 						formRegister={'register' + i}
+	// 						formControl={'control' + i}
+	// 						formErrors={'errors' + i}
+	// 						formHandleSubmit={'handleSubmit' + i}
+	// 					/> */}
+
+	// 						{/* <FormFieldGenerator
+	// 							formObject={getFormObject()}
+	// 							register={register}
+	// 							control={control}
+	// 							errors={errors}
+	// 							formIndex={i}
+	// 						/>
+
+	// 						<div>
+	// 							<h3>Form {i + 1}</h3>
+	// 							<button
+	// 								onClick={() => handleRemoveForm(i)}
+	// 								className='remove-feedback-form'
+	// 							>
+	// 								Remove
+	// 							</button>
+	// 						</div>
+	// 					</Fragment> */}
+	// 				{/* ))} */}
+	// 				<FormGenerator
+	// 					// key={i}
+	// 					// formIndex={i}
+	// 					formClass='feedback-form-wrapper'
+	// 					// formId={'feedback-form-' + i}
+	// 					onSubmit={onSubmit}
+	// 					formObject={getFormObject()}
+	// 					// handleRemoveForm={() => handleRemoveForm(i)}
+	// 					// isFeedbackForm={true}
+	// 					// formRegister={'register' + i}
+	// 					// formControl={'control' + i}
+	// 					// formErrors={'errors' + i}
+
+	// 					// formHandleSubmit={'handleSubmit' + i}
+	// 				/>
+	// 				{/* <div className='form-div'>
+	// 					<button onClick={handleAddForm} className='add-feedback-form'>
+	// 						Add another form
+	// 					</button>
+	// 				</div> */}
+
+	// 				{/* <div className='form-div'>
+	// 					<button onClick={e => SubmitForms(e)}>SUBMIT</button>
+	// 				</div> */}
+	// 			</div>
+	// 			{/* <div className='form-div'>
+	// 				<input type='submit' value='SUBMITTTT' />
+	// 			</div> */}
+	// 		</form>
+	// 	</div>
+	// );
 }
 
 export default FeedbackFormPage;
